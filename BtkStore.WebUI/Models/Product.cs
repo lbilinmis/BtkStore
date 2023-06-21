a24141569e4d44bf6e967aa7ac032fd420fb5742
lbilinmis <lbilinmis@gelisim.edu.tr>
2023-06-21 11:11:49 +0300

    Add project files.

---
+++ BtkStore.WebUI/Models/Product.cs
@@ -0,0 +1,9 @@
+﻿namespace BtkStore.WebUI.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; } = String.Empty;
+        public decimal Price { get; set; }
+    }
+}
---
+++ BtkStore.WebUI/Repository/RepositoryContext.cs
@@ -0,0 +1,14 @@
+﻿using BtkStore.WebUI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BtkStore.WebUI.Repository
+{
+    public class RepositoryContext : DbContext
+    {
+        public RepositoryContext(DbContextOptions<RepositoryContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
